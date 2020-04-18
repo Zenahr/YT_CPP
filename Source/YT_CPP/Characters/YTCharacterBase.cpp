c3197e276086cd1f36341095b55ecf02aedcb5b5
Zenahr Barzani <zenmatica@gmail.com>
2020-04-18 11:57:00 +0200

    removed tick in character

--- Source/YT_CPP/Characters/YTCharacterBase.cpp
+++ Source/YT_CPP/Characters/YTCharacterBase.cpp
@@ -7,7 +7,7 @@
 AYTCharacterBase::AYTCharacterBase()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 }
 
@@ -18,12 +18,6 @@ void AYTCharacterBase::BeginPlay()
 	
 }
 
-// Called every frame
-void AYTCharacterBase::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
 
 // Called to bind functionality to input
 void AYTCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
